eba760cf93a248d55e004e57c2dacc93465b301f
Taras fedorenko <fedorenkotv16@gmail.com>
2018-10-24 03:29:22 +0300

    Test Task on HW(I add everything, it will be useful in the future)

---
+++ NUnit.Tests1/NUnit.Tests1/LoginPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+
+namespace NUnit.Tests1
+{	
+	public class LoginPage
+	{
+		private readonly IWebDriver driver;
+		private readonly string url = "https://homsters.kz";
+
+		public LoginPage(IWebDriver driver)
+		{
+			this.driver = driver;
+			PageFactory.InitElements(driver, this);
+		}
+
+		[FindsBy(How = How.ClassName, Using = "b-complex-header__button--login")]
+		public IWebElement Loginbtn { get; set; }
+
+		[FindsBy(How = How.ClassName, Using = "js-user-name")]
+		public IWebElement Logintbx { get; set; }
+
+		[FindsBy(How = How.ClassName, Using = "js-user-password")]
+		public IWebElement Passwordtbx { get; set; }
+
+		[FindsBy(How = How.ClassName, Using = "b-button")]
+		public IWebElement Submitbtn { get; set; }
+
+		public void Navigate()
+		{
+			this.driver.Navigate().GoToUrl(this.url);
+		}
+
+		public void Entry(string email, string password)
+		{
+			this.Loginbtn.Click();
+			this.Logintbx.Clear();					//раптом там що є
+			this.Passwordtbx.Clear();	
+			this.Logintbx.SendKeys(email);			//передаєм текст у відповідні поля
+			this.Passwordtbx.SendKeys(password);
+			this.Submitbtn.Click();
+		}
+	}
+}
---
+++ NUnit.Tests1/NUnit.Tests1/TestLogin.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Text.RegularExpressions;
+
+namespace NUnit.Tests1
+{
+	[TestFixture]
+	public class TestLogin
+	{
+		private IWebDriver Driver { get; set; }
+																//ці значення можна отримувати 
+		private const string _email = "yirokamis@nyrmusic.com",	//безпосередньо після введення користувачем,
+			_password = "yirokamis@nyrmusic.com";               //необхідно для тесту на валідність!      
+
+		[SetUp]
+		public void SetupTest()
+		{
+			this.Driver = new ChromeDriver();			
+		}
+
+		[TearDown]
+		public void TeardownTest()
+		{
+			this.Driver.Quit();
+		}
+
+		[Test]//якщо я равильно зрозумів то це позитивний тест
+		public void Entry()
+		{
+			LoginPage loginPage = new LoginPage(this.Driver);
+			loginPage.Navigate();
+			loginPage.Entry(_email, _password);
+		}
+
+		const string _patternFORemail = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+				@"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
+
+		[Test]//якщо я равильно зрозумів то це negative тест
+		public void ValidEmail()
+		{	//успішно якщо нічого не чіпати, впаде якщо заберем наприклад собачку		
+			Assert.IsTrue(Regex.IsMatch(_email, _patternFORemail, RegexOptions.IgnoreCase));
+		}
+
+		const string _patternFORPassword = @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+
+		[Test]
+		public void ValidPassword()
+		{   //Впаде якщо нічого не чіпати, тут вже залежно яке правило в регулярний вираз засуниш
+			//якщо те, що для мейлу, то все буде ок (можна варіювати від вимоги тут же і помилки відловлювати)
+			Assert.IsTrue(Regex.IsMatch(_password, _patternFORPassword, RegexOptions.IgnoreCase));
+		}
+		/*	З приводу логування, я не зовсім зрозумів. Виходить можна просто
+		    в консоль все виводити і це теж буде логування, це якщо робити просто.
+			Я думаю це нікуди не годиться і потрібно створювати файл з логами за рахунок
+			сторонньої бібліотеки, але чітких інструкцій по такому підходу я не нажаль не знайшов, 
+			а те що знайшов, здалось складним як для новачка в даній темі.		 
+		 */
+	}
+}
